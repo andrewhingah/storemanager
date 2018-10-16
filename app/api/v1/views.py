9fcf0db9381e2bf2ab1ae7497001d4c75af8b308
Andrew Hinga <andrewhinga5@gmail.com>
2018-10-16 16:57:44 +0300

    [Feature #161250183] Implement get single product endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -11,7 +11,7 @@ parser.add_argument('name', required=True, help="Name cannot be blank")
 parser.add_argument('quantity', type=int, required=True, help="quantity cannot be blank")
 parser.add_argument('price', type=int, required=True, help="price cannot be blank")
 
-class Products(Resource):
+class AllProducts(Resource):
 	"""All products class"""
 	def get(self):
 		"""gets all products"""
@@ -33,3 +33,12 @@ class Products(Resource):
 		return make_response(jsonify({"message":"success",
 			"status":"created",
 			"sale":payload}), 201)
+
+class SingleProduct(Resource):
+	'''single product API'''
+	def get(self, id):
+		for key in products:
+			if key == id:
+				return products[id]
+		message = "Product not found"
+		return {"message":message}
