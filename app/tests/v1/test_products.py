97f950f613aef1955f7e65850b33f6f068f25bb2
Andrew Hinga <andrewhinga5@gmail.com>
2018-10-16 13:13:53 +0300

    [Chore #161209057] Create tests for a retrieving a single product

--- app/tests/v1/test_products.py
+++ app/tests/v1/test_products.py
@@ -27,11 +27,25 @@ class TestProducts(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_post(self):
+		"""test post a product
+		"""
 		product = {"name":"unga", "quantity":22, "price":100}
 		response = self.app.post('/api/v1/products',
 			data=json.dumps(product),
 			content_type='application/json')
 		self.assertEqual(response.status_code, 201)
+
+	def test_get_one_product(self):
+		"""test get a specific product by id
+		"""
+		response = self.app.get('/api/v1/product/1')
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_non_existing_product(self):
+		"""test a non existing product cannot be retrieved
+		"""
+		response = self.app.get('/api/v1/product/100')
+		self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
 	unittest.__main__
