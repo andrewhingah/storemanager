a6a348bc266f026894558d2c6d16b43ab91570dc
Andrew Hinga <andrewhinga5@gmail.com>
2018-10-17 20:25:18 +0300

    [Chore #161290311] Version api using blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,16 @@
 from flask import Flask, Blueprint
-from flask_restful import Resource, Api
 
 from instance.config import app_config
 
-from .api.v1.views.products_views import AllProducts, SingleProduct
-from .api.v1.views.sales_views import AllSales, SingleSale
-
-version1 = Blueprint('api', __name__)
-api = Api(version1)
-
 def create_app(config_name):
-	app=Flask(__name__, instance_relative_config=True)
+	app = Flask(__name__, instance_relative_config=True)
+
+	from .api.v1 import version1 as v1
+	app.register_blueprint(v1)
+
 	app.config.from_object(app_config[config_name])
 	app.config.from_pyfile('config.py')
 
-	app.register_blueprint(version1, url_prefix='/api/v1')
-	api.add_resource(AllProducts, '/products')
-	api.add_resource(SingleProduct, '/product/<int:product_id>')
-	api.add_resource(AllSales, '/sales')
-	api.add_resource(SingleSale, '/sale/<int:sale_id>')
-
 	return app
+
+
